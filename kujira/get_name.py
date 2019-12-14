4e13ecc0cb317e38e1982587b832cf909b6cd282
Pyunghyuk Yoo <yoophi@gmail.com>
2019-12-15 00:16:24 +0900

    get typhoon name

--- kujira/get_name.py
+++ kujira/get_name.py
@@ -1,4 +1,6 @@
 import os
+import json
+import random
 
 
 class TYPHOON_TYPE:
@@ -10,3 +12,11 @@ class TYPHOON_TYPE:
 def get_typhoon_name(type=TYPHOON_TYPE.ALL):
     if type == TYPHOON_TYPE.ALL:
         data_file = os.path.join(os.path.dirname(__file__), 'typhoon-names.json')
+        with open(data_file, 'r') as f:
+            data = json.load(f)
+
+    return random.choice(data)
+
+
+if __name__ == '__main__':
+    print(json.dumps(get_typhoon_name()))
